1949f18d47df68812a7d912560ae0e1e88b347ab
lsz <com>
2020-05-05 21:03:41 +0800

    provider集群，配置consumer

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableEurekaClient
+public class PaymentMain8002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class, args);
+    }
+
+}
