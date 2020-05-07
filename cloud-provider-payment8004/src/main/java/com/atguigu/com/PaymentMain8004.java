06f45eec61f55d3ecf5eb2554c5c0e89c446b338
lsz <com>
2020-05-07 22:21:56 +0800

    zookeeper（3.4.9）注册服务

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/com/PaymentMain8004.java
@@ -0,0 +1,17 @@
+package com.atguigu.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2
+public class PaymentMain8004 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+
+}
---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/com/controller/PaymentController.java
@@ -0,0 +1,21 @@
+package com.atguigu.com.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping("/payment/zk")
+    public String paymentZk() {
+        return "spring cloud with zookeeper: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+
+
+}
