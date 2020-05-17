b4da4b2e124ebb1c8ca14a563bb77267f06ac890
lsz <com>
2020-05-17 16:51:46 +0800

    open feign的配置

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeign80.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableFeignClients
+public class OrderFeign80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeign80.class, args);
+    }
+
+}
