87fabf39b285a07bccfea0a9fed1da3c356c4244
lsz <com>
2020-05-17 15:21:19 +0800

    自定义ribbon IRule

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -1,13 +1,16 @@
 package com.atguigu.springcloud;
 
+import com.atgugu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
 
     public static void main(String[] args) {
