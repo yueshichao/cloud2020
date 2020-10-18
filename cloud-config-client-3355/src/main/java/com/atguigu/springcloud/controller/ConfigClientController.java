be160ace40d41c07b3eeed00d26f5992521a91e1
zr <zheergen@suiyi.com.cn>
2020-10-18 18:53:56 +0800

    config客户端手动刷新

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,10 +1,12 @@
 package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope// 配置修改后执行命令（可以刷新配置）： curl -X POST "http://localhost:3355/actuator/refresh"
 public class ConfigClientController {
 
     @Value("${config.info}")
