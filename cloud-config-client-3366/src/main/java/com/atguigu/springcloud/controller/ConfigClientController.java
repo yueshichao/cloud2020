eeb6da34209c10524d71371f40982c0cbf95a0a2
zr <zheergen@suiyi.com.cn>
2020-10-25 22:12:31 +0800

    Bus 动态刷新（unfinished!）

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    public String configInfo() {
+        return "serverPort: " + serverPort + "\t\n\nconfigInfo: " + configInfo;
+    }
+
+}
