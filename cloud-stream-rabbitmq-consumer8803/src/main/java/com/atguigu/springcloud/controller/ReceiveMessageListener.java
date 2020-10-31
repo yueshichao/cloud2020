a8fda7ab51614dbf2c20f92a077f2305873b85c9
zr <zheergen@suiyi.com.cn>
2020-10-31 10:31:59 +0800

    Stream 消费组

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/controller/ReceiveMessageListener.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableBinding(Sink.class)
+@Slf4j
+public class ReceiveMessageListener {
+
+    @Value("${server.port}")
+    private String port;
+
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        log.info("消费者2号， ---> 接收到消息：{} \t port = {}", message.getPayload(), port);
+    }
+
+
+}
